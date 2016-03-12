d66bdd496bb59a68d96e934b88d47b64506c322e
javimtib92 <javimtib92@gmail.com>
2016-03-12 11:57:49 +0100

    added calendar for assigned rooms

--- www/components/assignations/assignations.controller.js
+++ www/components/assignations/assignations.controller.js
@@ -5,6 +5,7 @@
         .controller('AssignationsCtrl', AssignationsCtrl)
         .controller('AssignationsDetailCtrl', AssignationsDetailCtrl)
         .controller('AssignationsCreateCtrl', AssignationsCreateCtrl)
+        .controller('CalendarCtrl', CalendarCtrl)
         .run(['routerHelper', function (routerHelper) {
             routerHelper.configureStates(getStates());
         }]);
@@ -51,6 +52,72 @@
             }
           );
         }
+    }
+
+    function CalendarCtrl($http, AppConfig) {
+      var vm = this;
+      vm.options = {
+        calendarView: 'week',
+        calendarDate: moment(new Date()),
+        calendarTitle: ''
+      }
+
+      vm.selectedDate = moment();
+      vm.hotels = [
+        {id: 1, name: 'Balearic Resorts Palma'},
+        {id: 2, name: 'Balearic Resorts Arta'},
+        {id: 3, name: 'Balearic Resorts Fornalutx'},
+        {id: 4, name: 'Balearic Resorts Inca'},
+        {id: 5, name: 'Balearic Resorts Manacor'},
+        {id: 6, name: 'Balearic Resorts Campos'}
+      ]
+
+      vm.selected = {
+        id:1,
+        name: 'Balearic Resorts Palma'
+      };
+      //TODO pasarle fechas y hotel
+      vm.loadData = function() {
+        var copy = moment(vm.selectedDate);
+
+        var start_date = copy.subtract(vm.selectedDate.day(), 'days').format('YYYY-MM-DD');
+        var end_date = copy.add(7 - vm.selectedDate.day() - 1 + 7, 'days').format('YYYY-MM-DD');
+
+        $http({
+          'url': AppConfig.BASE_API_URL + '/assignements',
+          'method': 'GET',
+          'params': {
+            'start_date': start_date,
+            'end_date': end_date,
+            'hotel_id': vm.selected.id
+          }
+        })
+          .then(
+            function(data) {
+              console.log(data);
+              var data = data.data.data;
+              var result = [];
+
+              var i = 0;
+              for(; i < data.length; i++) {
+                var object = {
+                  title: 'Room number: ' + data[i].room_id, // The title of the event
+                  type: 'info',
+                  startsAt: data[i].start_date,
+                  endsAt: data[i].end_date,
+                  editable: false, // If edit-event-html is set and this field is explicitly set to false then dont make it editable.
+                  deletable: false,
+                }
+
+                result.push(object);
+              }
+
+              vm.events = result;
+            }
+          )
+      }
+
+      vm.loadData();
     }
 
     function getStates() {
